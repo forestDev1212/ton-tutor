3908642640449392c507e144318a9f09d96b1f2d
forestDev1212 <alexeidorman@gmail.com>
2024-06-10 17:44:36 +0900

    feat: update step 9 for transfer tokens

--- step8.ts
+++ step8.ts
@@ -1,22 +1,26 @@
+import { config } from 'dotenv';
 import { getHttpEndpoint } from "@orbs-network/ton-access";
 import { mnemonicToWalletKey } from "@ton/crypto";
 import { WalletContractV4, TonClient, fromNano } from "@ton/ton";
 
+config(); // Load environment variables from .env file
+
 async function main() {
-  // open wallet v4 (notice the correct wallet version here)
-  const mnemonic = "shy awkward spare sad flat visual weasel message tonight maze wreck skill access donkey asset super erase angle country quote armed kit flock connect"; // your 24 secret words (replace ... with the rest of the words)
+  const mnemonic = process.env.MNEMONIC;
+
+  if (!mnemonic) {
+    throw new Error("Mnemonic is undefined. Please set the MNEMONIC environment variable.");
+  }
+
   const key = await mnemonicToWalletKey(mnemonic.split(" "));
   const wallet = WalletContractV4.create({ publicKey: key.publicKey, workchain: 0 });
 
-  // initialize ton rpc client on testnet
   const endpoint = await getHttpEndpoint({ network: "testnet" });
   const client = new TonClient({ endpoint });
 
-  // query balance from chain
   const balance = await client.getBalance(wallet.address);
   console.log("balance:", fromNano(balance));
 
-  // query seqno from chain
   const walletContract = client.open(wallet);
   const seqno = await walletContract.getSeqno();
   console.log("seqno:", seqno);
